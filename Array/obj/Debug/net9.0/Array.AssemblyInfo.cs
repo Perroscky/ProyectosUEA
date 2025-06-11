357769a8f802e758d54c3b430379ff9fba52ecbe
Perroscky <la.samaniegos@uea.edu.ec>
2025-06-10 22:16:58 -0500

    actualizacion 4

--- Array/obj/Debug/net9.0/Array.AssemblyInfo.cs
+++ Array/obj/Debug/net9.0/Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6227853fa820ed1a9e395955a0af29d4b2152d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fbd25777a5a77a037f30650f4ca9f02240d18ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
