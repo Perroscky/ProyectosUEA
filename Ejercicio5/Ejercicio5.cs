7b18bce7b4b23f1be940c37f6983868f940a176a
Perroscky <la.samaniegos@uea.edu.ec>
2025-06-26 21:50:27 -0500

    Actualizacion 13

---
+++ Ejercicio5/Ejercicio5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear y llenar la lista con los números del 1 al 10
+        List<int> numeros = new List<int>();
+        for (int i = 1; i <= 10; i++)
+        {
+            numeros.Add(i);
+        }
+
+        // Mostrar en orden inverso separados por comas
+        Console.WriteLine("Números en orden inverso:");
+        for (int i = numeros.Count - 1; i >= 0; i--)
+        {
+            Console.Write(numeros[i]);
+            if (i != 0)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine(); // Salto de línea final
+    }
+}
