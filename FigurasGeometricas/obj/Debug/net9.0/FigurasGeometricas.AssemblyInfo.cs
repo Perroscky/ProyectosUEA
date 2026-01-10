37307f7be65d14715825cb12ed4861a464a7f803
Perroscky <la.samaniegos@uea.edu.ec>
2026-01-10 09:05:48 -0500

    Actualizacion

--- FigurasGeometricas/obj/Debug/net9.0/FigurasGeometricas.AssemblyInfo.cs
+++ FigurasGeometricas/obj/Debug/net9.0/FigurasGeometricas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FigurasGeometricas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4475f4d8e24bd4548762a2ade7a6b7cf9f72cff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e55c22daae5e69bc12a10e10877ab05d3e123d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FigurasGeometricas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FigurasGeometricas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
