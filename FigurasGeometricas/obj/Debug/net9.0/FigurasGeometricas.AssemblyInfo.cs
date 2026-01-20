6aed681394fd6993ec43820ff19a05137793e9f5
Perroscky <la.samaniegos@uea.edu.ec>
2026-01-19 19:06:41 -0500

    Actualizacion 35

--- FigurasGeometricas/obj/Debug/net9.0/FigurasGeometricas.AssemblyInfo.cs
+++ FigurasGeometricas/obj/Debug/net9.0/FigurasGeometricas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FigurasGeometricas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37307f7be65d14715825cb12ed4861a464a7f803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fda647a39dcc164fd72a86e5d13b6ad5bee134")]
 [assembly: System.Reflection.AssemblyProductAttribute("FigurasGeometricas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FigurasGeometricas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ semana7/script.js
@@ -0,0 +1,11 @@
+// 1. Manejo del DOM
+document.getElementById()
+createElement()
+appendChild()
+
+// 2. Manipulación de arreglos
+productos.push()
+productos.forEach()
+
+// 3. Validación de datos
+trim(), parseFloat()
